796b4791025f8ab3e0027afb5cc2956f10db5628
Rodrigo Dirube <rodrigodirube5@gmail.com>
2022-09-12 18:23:05 -0300

    post done, problems with images

--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('signup', views.signup, name = 'signup'),
     path('signin', views.signin, name = 'signin'),
     path('logout', views.logout, name = 'logout'),
-    path('setting', views.settings, name = 'setting')
+    path('setting', views.settings, name = 'setting'),
+    path('upload', views.upload, name = 'upload')
+
 
 ]
