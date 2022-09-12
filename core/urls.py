c31cffdeb6fd4c9f3d3ea024b87b38c1303f2fbb
Rodrigo Dirube <rodrigodirube5@gmail.com>
2022-09-12 11:22:01 -0300

    social_book init

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path;
+from . import views;
+from .views import index;
+
+# Se instancian los distintos / o views de la pagina
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('signup', views.signup, name = 'signup'),
+    path('signin', views.signin, name = 'signin'),
+    path('logout', views.logout, name = 'logout'),
+    path('setting', views.settings, name = 'setting')
+
+]
